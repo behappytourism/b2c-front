7b53ffabf7fc9d855355603bb812bc5b091e0868
rayyan1902 <rayyanakhter1905@gmail.com>
2024-09-23 16:31:21 +0530

    star component commented

--- src/components/Attraction/SectionGridFilterAttractionCard.tsx
+++ src/components/Attraction/SectionGridFilterAttractionCard.tsx
@@ -71,7 +71,7 @@ const SectionGridFilterAttractionCard: FC<SectionGridFilterCardProps> = ({
         <TabFilters />
       </div> */}
       <div className="grid grid-cols-1 md:gap-8 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-        {data?.attractions?.data.map((excursion, indx) => (
+        {data?.attractions?.data.slice(0, 12).map((excursion, indx) => (
           <ExperiencesCard key={excursion._id} data={excursion} />
         ))}
       </div>
