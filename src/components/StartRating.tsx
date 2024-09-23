3bcbde5009164616c027b123f3fb9b3acb3244b2
rayyan1902 <rayyanakhter1905@gmail.com>
2024-09-23 16:27:17 +0530

    star component commented

--- src/components/StartRating.tsx
+++ src/components/StartRating.tsx
@@ -21,10 +21,10 @@ const StartRating: FC<StartRatingProps> = ({
       {/* <div className="pb-[2px]">
         <StarIcon className="md:w-[18px] w-[12px] h-[12px] md:h-[18px] text-orange-500" />
       </div> */}
-      <span className="font-medium text-xs md:text-sm">{point}</span>
+      {/* <span className="font-medium text-xs md:text-sm">{point}</span>
       <span className="text-neutral-500 text-xs md:text-sm dark:text-neutral-400">
         ({reviewCount})
-      </span>
+      </span> */}
     </div>
   );
 };
