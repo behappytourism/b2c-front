187c8d97429426ff315ffc0026daae719d5d873f
rayyan1902 <rayyanakhter1905@gmail.com>
2024-09-23 16:21:25 +0530

    start icon commented

--- src/components/StartRating.tsx
+++ src/components/StartRating.tsx
@@ -18,9 +18,9 @@ const StartRating: FC<StartRatingProps> = ({
       className={`nc-StartRating flex items-center space-x-1 text-sm  ${className}`}
       data-nc-id="StartRating"
     >
-      <div className="pb-[2px]">
+      {/* <div className="pb-[2px]">
         <StarIcon className="md:w-[18px] w-[12px] h-[12px] md:h-[18px] text-orange-500" />
-      </div>
+      </div> */}
       <span className="font-medium text-xs md:text-sm">{point}</span>
       <span className="text-neutral-500 text-xs md:text-sm dark:text-neutral-400">
         ({reviewCount})
