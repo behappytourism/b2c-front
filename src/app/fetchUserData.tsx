7c83f2fbacfb036c46a37a0bd7413218e33b3aca
Rayyan Akhter <rayyanakhter1905@gmail.com>
2025-03-25 14:18:40 +0530

    bug fixed

--- src/app/fetchUserData.tsx
+++ src/app/fetchUserData.tsx
@@ -1,39 +1,40 @@
 'use client'
 
-import { AppDispatch } from "@/redux/store";
-import { useDispatch } from "react-redux";
-import { setInitialData } from "@/redux/features/initialsSlice";
-import { useEffect } from "react";
+import { AppDispatch, RootState } from "@/redux/store";
+import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "@/redux/features/usersSlice";
-
-
-
+import { useEffect } from "react";
 
 const FetchUserData = () => {
-    const dispatch = useDispatch<AppDispatch>()
-    const jwtToken = typeof window !== "undefined" && localStorage.getItem("random-string") ? localStorage.getItem("random-string") : "";
+    const dispatch = useDispatch<AppDispatch>();
+    const { jwtToken } = useSelector((state: RootState) => state.users);
+
     useEffect(() => {
         async function getUserData() {
             try {
                 const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/v1/users/my-account`, {
-                    next: { revalidate: 10 }, headers: {
+                    next: { revalidate: 10 }, 
+                    headers: {
                         authorization: `Bearer ${jwtToken}`,
                     },
-                })
+                });
 
-                dispatch(setUser({ user: await response.json(), jwtToken: jwtToken }));
+                if (response.status !== 200) {
+                    localStorage.removeItem("random-string");
+                } else {
+                    dispatch(setUser({ user: await response.json(), jwtToken: jwtToken }));
+                }
+
             } catch (err: any) {
                 console.log(err, "user-data");
+                localStorage.removeItem("random-string");
             }
         }
-        getUserData()
 
-    }, [dispatch])
-
-
+        getUserData();
+    }, [dispatch, jwtToken]);
 
     return <></>;
-
 }
 
-export default FetchUserData
+export default FetchUserData;
